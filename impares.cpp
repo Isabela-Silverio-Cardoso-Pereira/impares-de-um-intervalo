700c68454ff19784e900e2022b08f9f2081a0f52
Isabela-Silverio-Cardoso-Pereira <isabelasilverio.c@gmail.com>
2023-11-09 13:18:03 -0300

    primeiro commit

---
+++ impares.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Imprimir os números ímpares inteiros de X a Y
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    int x, y;
+    impar = 1;
+    cout << "digite x";
+    cin >> x;
+    cout << "digite y";
+    cin >> y;
+    
+    int i = 1;
+    
+    while(i<=y){
+        
+        
+        
+        i++;
+        if(i%2==1){
+        
+         cout << i;}
+       
+        
+    
+    }
+    
+    
+}
